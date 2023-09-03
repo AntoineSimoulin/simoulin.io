733c3d20230a4a381149955c15203f923dff45a2
AntoineSimoulin <antoine.simoulin@gmail.com>
2023-09-03 12:00:17 -0500

    :books: Add reviewer service

--- src/views/Communications.vue
+++ src/views/Communications.vue
@@ -155,6 +155,7 @@ export default {
                   role="tabpanel"
                   aria-labelledby="course-pills-tab-3">
                   <ul>
+                    <li>Reviewer Neurips Datasets and Benchmarks’23</li>
                     <li>Reviewer ACL’20, EMNLP’20</li>
                     <li>
                       Hackathon Winner. PyTree, a PyTorch package for recursive
