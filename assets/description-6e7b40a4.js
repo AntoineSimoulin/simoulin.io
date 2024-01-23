47eb22cc3653bd3c6e2b2701ecb0a61dd1a0bcb4
AntoineSimoulin <antoine.simoulin@gmail.com>
2024-01-22 21:08:37 -0600

    Updates

---
+++ assets/description-6e7b40a4.js
@@ -0,0 +1,20 @@
+const e=`# Course Description
+
+Welcome to the **Master 2 Mathématiques et Informatique pour la Data Science (M2 MIDS) from Paris University.**
+
+The course is part of the [M2 MIDS](https://m2mids.github.io/m2mids/) .It introduces statistical methods for Natural Language Processing, including text vectorization, word embeddings, and sequence models. In addition, the course includes practical exercises that cover web scraping, regular expressions, and visualization. The labs include common use cases such as text classification or topic mining. Finally, the course gives an overview about open-source tools for nlp such as nlp libraries, intelligibility methods or deep learning frameworks.
+
+## What you’ll learn
+
+- Text Vectorization
+- Word Embeddings
+- Sequence Models
+- Web Scraping
+- Regular Expressions
+- Data Vizualization For Text-
+- Text Classification
+- Topic Mining
+- Open-Source Tools
+- NLP Libraries
+- Intelligibility Methods
+- Deep Learning Frameworks`;export{e as default};
--- assets/merge-sort-e743979d.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/merge-sort-6c0ed01a.svg";export{s as _};
