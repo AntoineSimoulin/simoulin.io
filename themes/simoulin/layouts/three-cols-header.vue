6c5f905313ac5b94f7fe6d2ab431f91de002c923
AntoineSimoulin <antoine.simoulin@gmail.com>
2026-02-22 16:48:25 -0600

    feat(layout): add three-cols-header layout for slides

---
+++ themes/simoulin/layouts/three-cols-header.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  align?: 'top' | 'bottom' | 'center' | 'stretch'
+}>()
+
+const alignClass = computed(() => {
+  switch (props.align) {
+    case 'top': return 'items-start'
+    case 'bottom': return 'items-end'
+    case 'center': return 'items-center'
+    case 'stretch': return 'items-stretch'
+    default: return 'items-stretch'
+  }
+})
+</script>
+
+<template>
+  <div class="slidev-layout three-cols-header w-full h-full flex flex-col">
+    <div class="col-span-3 mb-4">
+      <slot />
+    </div>
+    <div class="grid grid-cols-3 gap-6 flex-1 min-h-0" :class="alignClass">
+      <div class="col-left h-full flex flex-col">
+        <slot name="left" />
+      </div>
+      <div class="col-center h-full flex flex-col">
+        <slot name="center" />
+      </div>
+      <div class="col-right h-full flex flex-col">
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
