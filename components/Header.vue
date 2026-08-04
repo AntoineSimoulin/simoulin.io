29878aa22f801165737d633a9985bc43383dd3e9
AntoineSimoulin <antoine.simoulin@gmail.com>
2026-08-04 08:15:56 -0500

    fix(css): fix PostCSS @import order and constrain mobile menu SVG dimensions

--- components/Header.vue
+++ components/Header.vue
@@ -13,7 +13,7 @@
         <!-- Mobile Dropdown -->
         <div class="dropdown">
           <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="width: 20px; height: 20px;"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><NuxtLink to="/">About</NuxtLink></li>
